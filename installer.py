13628801bdfb1087524d12380ea257deefa36566
slovakianedits <editzslovakian@gmail.com>
2024-11-14 19:02:09 +0100

    Update installer.py

--- installer.py
+++ installer.py
@@ -1,9 +1,5 @@
 import time
 time.sleep(2)
-if downloaded:
-  print("This file is downloaded!")
-  time.sleep(5)
-else:
-  print("nothing")
-  time.sleep(5)
+print("This file is downloaded!")
+time.sleep(5)
   
